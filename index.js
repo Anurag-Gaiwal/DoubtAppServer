962fbdcc0eaad35d84d1e214458fa04a7b008f39
Anurag Gaiwal <123549836+Anurag-Gaiwal@users.noreply.github.com>
2024-07-02 09:44:54 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient} = require("mongodb");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.post("/save",(req,res)=>{
+	const url = "mongodb+srv://anuraggaiwal0:ohQGNHacYqOvjHmp@cluster0.8tsfb3f.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+	const client = new MongoClient(url);
+	const db = client.db("doubt25june24");
+	const coll = db.collection("Student");
+	const record = {"name":req.body.name,"doubt":req.body.doubt};
+	coll.insertOne(record)
+	.then(result=>res.send(result))
+	.catch(error=>res.send(error));
+});
+
+app.listen(9000,()=>{console.log("Ready @ 9000");});
